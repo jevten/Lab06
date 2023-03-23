59e5b07e34cfc84452973ea11d2235ac07f797fd
zmunir1 <90813758+zmunir1@users.noreply.github.com>
2023-03-22 21:23:22 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,37 +1,68 @@
-#Jevan Tenaglia
-def encode(password):
-    result =""
-    for i in range (len(password)):
-        if int(password[i])<=6:
-            result += str(int(password[i]) + 3)
-        elif int(password[i]) == 7:
-            result += "0"
-        elif int(password[i]) == 8:
-            result += "1"
-        elif int(password[i]) == 9:
-            result += "2"
-    print(f'"{password}" will become "{result}" after encoding')
+def encode(string):
+    password = ""
+    for num in range(len(string)):
+        if int(string[num]) == 0:
+            password += "3"
+        elif int(string[num]) == 1:
+            password += "4"
+        elif int(string[num]) == 2:
+            password += "5"
+        elif int(string[num]) == 3:
+            password += "6"
+        elif int(string[num]) == 4:
+            password += "7"
+        elif int(string[num]) == 5:
+            password += "8"
+        elif int(string[num]) == 6:
+            password += "9"
+        elif int(string[num]) == 7:
+            password += "0"
+        elif int(string[num]) == 8:
+            password += "1"
+        elif int(string[num]) == 9:
+            password += "2"
 
-def decode(password):
-    str_password = str(password)
-    dencoded_string = ""
-    for num in str_password:
-        dencoded_value = str((int(num) - 3) % 10)
-        dencoded_string += dencoded_value
-    return dencoded_string
+def decode(string):
+    string_value = ""
+    password = str(string)
+    for num in password:
+        result = str(int(num) - 3)
+        string_value += result
+        if result == "0":
+            string_value += "7"
+        elif result == "1":
+            string_value += "8"
+        elif result == "2":
+            string_value += "9"
+    return string_value
+
+
 
 if __name__ == '__main__':
-    password =""
+    string = ""
     while True:
-        print("Menu\n-------------\n1. Encode\n2. Decode\n3. Quit")
-        selection = int(input("Please enter an option:"))
-        if selection == 1:
-            password = input("Please enter your password to encode:")
-            password = encode(password)
-            print("Your password has been encoded and stored!")
-        if selection == 2:
-            decode_pass = decode(password)
-            print(f"The encoded password is {password}, and the original password is {decode_pass}")
-        if selection == 3:
-            quit()
+        print('Menu')
+        print('-------------')
+        print('1. Encode')
+        print('2. Decode')
+        print('3. Quit')
+        option = int(input('Please enter an option:'))
+        if option == 1:
+            string = input('Please enter your password to encode:')
+            string = encode(string)
+            print('Your password has been encoded and stored!')
+        if option == 2:
+            dec_string = decode(string)
+            print(f'The encoded password is {string}, and the original password is {dec_string}')
+        if option == 3:
+            break
+
+
+
+
+
+
+
+
+
 
